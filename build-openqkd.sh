7b1b7dc7cb2b8d45b112db570ea77058717ea60e
Kaiduan Xie <kaiduanx@goodstartsoft.com>
2020-12-12 11:08:09 -0500

    Change directory in build script.

--- build-openqkd.sh
+++ build-openqkd.sh
@@ -2,7 +2,7 @@
 
 set -x
 autoreconf -i -f -v
-./configure --prefix=/usr/local/openvpn --enable-debug OPENSSL_CFLAGS="-I/home/b43s/qkd-work/openssl-install/openssl/include" OPENSSL_LIBS="-L/home/b43s/qkd-work/openssl-install/openssl/lib -Wl,-rpath=/usr/local/openvpn/lib  -lssl -lcrypto -lopenqkd -lcurl -ljson-c"
+./configure --prefix=/usr/local/openvpn --enable-debug OPENSSL_CFLAGS="-I/home/b43s/qkd-test/openssl-install/openssl/include" OPENSSL_LIBS="-L/home/b43s/qkd-test/openssl-install/openssl/lib -Wl,-rpath=/usr/local/openvpn/lib  -lssl -lcrypto -lopenqkd -lcurl -ljson-c"
 make clean
 make
-make DESTDIR=/home/b43s/qkd-work/stage-openqkd install
+make DESTDIR=/home/b43s/qkd-test/stage-openqkd install
